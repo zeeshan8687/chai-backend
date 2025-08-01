474daa9c5dc3f9c29787974d6eb9c95cd1fff304
zeeshan8687 <zeeshan@nextolive.com>
2025-08-01 12:16:03 +0530

    Implement user account management features: change password, get current user, update account details, and update user images

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { registerUser,  loginUser, logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
+import 
+{
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails
+}
+from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,7 +34,10 @@ router.route("/login").post(loginUser)
 
 
 //secured routes
-router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 export default router
