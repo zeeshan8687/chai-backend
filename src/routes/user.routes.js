81b14750606dc5df2e0e1e6dfef170721eb1287a
zeeshan8687 <zeeshan@nextolive.com>
2025-08-01 17:50:05 +0530

    Add user channel profile and watch history endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,7 +7,10 @@ import
     refreshAccessToken,
     changeCurrentPassword,
     getCurrentUser,
-    updateAccountDetails
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile
 }
 from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -39,5 +42,10 @@ router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 
 export default router
