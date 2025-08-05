de7b0721b0f7bfd5750b45d4ed763f55c4d06434
zeeshan8687 <zeeshan@nextolive.com>
2025-08-05 12:05:32 +0530

    Implement video management features: add video model, routes, and controller with upload functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js"
+import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -425,8 +425,6 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
         new ApiResponse(200, channel[0], "User channel fetched successfully")
     )
 })
-
-
 
 
 const getWatchHistory = asyncHandler(async(req, res) => {
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -25,5 +25,21 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
+const deleteOnCloudinary = async (public_id, resource_type="image") => {
+    try {
+        if (!public_id) return null;
 
-export {uploadOnCloudinary};
+        //delete file from cloudinary
+        const result = await cloudinary.uploader.destroy(public_id, {
+            resource_type: `${resource_type}`
+        });
+    } catch (error) {
+        return error;
+        console.log("delete on cloudinary failed", error);
+    }
+};
+
+
+
+export { uploadOnCloudinary, deleteOnCloudinary };
+
